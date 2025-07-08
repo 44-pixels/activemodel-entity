05796118077588e05ffffde1e1f087910396a5f4
HolyWalley <yakau.krasnou@44pixels.ai>
2025-07-08 12:51:27 +0200

    [BAC-336] Remove on_load hook

--- lib/active_model/entity/railtie.rb
+++ lib/active_model/entity/railtie.rb
@@ -5,13 +5,11 @@ module ActiveModel
     # Railtie to integrate with Rails and ActiveJob
     class Railtie < Rails::Railtie
       initializer "activemodel_entity.configure_active_job_serializer" do
-        ActiveSupport.on_load(:active_job) do
-          require "active_model/entity/active_job"
+        require "active_model/entity/active_job"
 
-          if defined?(ActiveJob::Serializers::ActiveModelSerializer)
-            Rails.application.config.to_prepare do
-              Rails.application.config.active_job.custom_serializers << ActiveJob::Serializers::ActiveModelSerializer
-            end
+        if defined?(ActiveJob::Serializers::ActiveModelSerializer)
+          Rails.application.config.to_prepare do
+            Rails.application.config.active_job.custom_serializers << ActiveJob::Serializers::ActiveModelSerializer
           end
         end
       end
